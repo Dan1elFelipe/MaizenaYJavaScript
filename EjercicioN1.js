f443169ee3295efa8bf4608e7d7351873631f259
Dan1elFelipe <danielfelipeluborealpe@gmail.com>
2023-05-12 13:23:08 -0500

    nuevos cambios

---
+++ EjercicioN1.js
@@ -0,0 +1,1 @@
+//hacer una factura con prompt y alert
---
+++ EjercicioN3.js
@@ -0,0 +1,21 @@
+//crear objeto para registro de motopajero
+
+const usuario = [
+    {
+     nombre: prompt("Ingresa motero: "),
+     moto: prompt("Ingresa moto: "),
+     marca: prompt("ingresa marca: "),
+    },
+    {
+         nombre: prompt("Ingresa motero: "),
+         moto: prompt("Ingresa moto: "),
+         marca: prompt("ingresa marca: "),
+        },
+        {
+             nombre: prompt("Ingresa motero: "),
+             moto: prompt("Ingresa moto: "),
+             marca: prompt("ingresa marca: "),
+            }
+]
+
+console.table (usuario);
